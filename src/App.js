87c2af7411f0b41e81fd52d6c892f2ab31750ec9
Alex Peña <attn.alex.p@gmail.com>
2021-01-05 18:52:42 -0800

    cannot get form to accept input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
+// Import just the "Component" part of the React library
+import React, { Component } from 'react';
+// Create a web app with a top level App component
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// App component should…
+// have thingList data stored in its state.
+// render 3 nested components
+// Header
+// ThingList
+// Footer
+class App extends Component {
+  // Create a constructor to create initial state and add it to props
+  constructor(props) {
+    super(props);
+    this.state = {
+      // thingList should be an array of plain old JavaScript objects (aka POJO) that represent a thing that has a name.
+      // E.g. {name:’rake’}
+      things: [
+        { id: 1, title: "Thing 1", description: "Was named first" },
+        { id: 2, title: "Thing 2", description: "Was named second" },
+      ]
+    }
+    this.thingMaker = this.thingMaker.bind(this);
+  };
+
+  thingMaker(thing) {
+    const newThings = [...this.state.things, thing];
+    this.setState({newThings});
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* Give Components the access the whatever state is needed */}
+        <Header thingCount={this.state.things.length} />
+        <ThingList things={this.state.things} thingHandler={this.thingMaker} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
+
+// Header component should…
+// receive a things count as a prop
+// display a heading
+// display the current count of things
+function Header(props) {
+  // Display the number of things by accessing the props
+  return <h1>Things Total: {props.thingCount}</h1>
+}
+
+
+// ThingList component should…
+// receive a list of things as a prop
+// receive a function to call when a new thing is created.
+// Display an unordered list composed of ThingItem components
+// Display a form that allows creation of a thing
+// When user creates new thing the rest of application should update appropriately.
+// Header thing count should update
+// ThingList should add a new ThingItem to end of list
+// ThingItem component should…
+// receive a name as a prop
+
+// ThingsList needs to be a class that extends Component to use state without hooks
+class ThingList extends Component {
+
+  render() {
+    return (
+      <div>
+        <ThingForm thingMaker={this.props.thingMaker} />
+        <ul>
+          {this.props.things.map(thing => (
+            <ThingItem key={thing.id} title={thing.title} description={thing.description} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+class ThingForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { title:'', description:'' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    // alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+    this.props.thingMaker(this.state)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Title:
+          <input type="text" value={this.state.title} onChange={this.handleChange} />
+          Description:
+          <input type="text" value={this.state.description} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+function ThingItem(props) {
+  return <li> {props.title}</li>
+}
+
+// Footer component should…
+// Display some placeholder text (e.g. lorem ipsum)
+function Footer(props) {
+  return <h3>Footer Comming Soon </h3>
+}
 export default App;
